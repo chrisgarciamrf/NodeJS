e1c69ff2266748e7b66e9aced64caf7b2739db7b
penchochris <garabchris@gmail.com>
2018-04-03 21:20:03 +0200

    Module#2-CoreConcepts

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,10 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'Wes';
-    if(req.name === 'Wes') {
-        throw Error(`That's an stupid name!`);
-    }
-    next();
-};
 exports.homePage = (req, res) => {
     res.render('index', {
        name: req.name 
