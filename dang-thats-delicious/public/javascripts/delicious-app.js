f2a3ebe21f23f1ffb7a26c92344b29620b36a174
penchochris <garabchris@gmail.com>
2018-04-08 21:13:25 +0200

    Module#5-Geolocation

--- dang-thats-delicious/public/javascripts/delicious-app.js
+++ dang-thats-delicious/public/javascripts/delicious-app.js
@@ -1,3 +1,10 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+import autocomplete from './modules/autocomplete';
+
+const address = document.querySelector('#address');
+const lat = document.querySelector('#lat');
+const lng = document.querySelector('#lng');
+
+autocomplete(address, lat, lng);
